a86747c1ec6461696f5ac8b5496e7a5507e0d20c
Nickunj Chopra <nickunjchopra@gmail.com>
2022-07-23 15:42:23 +0530

    Figured out tests and how to run them from the shell, wrote more tests for the polls app's Index view, made a silly mistake and spent an embarassing amount of time trying to figure it out.

--- refresher/polls/tests.py
+++ refresher/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -29,3 +30,50 @@ class QuestionModelTests(TestCase):
     two_days_ago = timezone.now() - datetime.timedelta(days=2)
     old_question = Question(pub_date=two_days_ago)
     self.assertIs(old_question.was_published_recently(), False)
+
+
+def create_question(question_text, num_days):
+  '''Shortcut function to quickly create dummy questions for tests.
+  question_text is the title of the question, and num_days is an int value for how
+  many days in the past/future the question's pub_date is. Use negative values for past dates.'''
+  time = timezone.now() + datetime.timedelta(days=num_days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+
+  def test_no_questions(self):
+    '''In the case of no questions, an appropriate message is displayed.'''
+    response = self.client.get(reverse('polls:index'))
+    self.assertEqual(response.status_code, 200)
+    # The error message might be any string, but will definitely say "no polls"
+    self.assertContains(response, 'No polls')
+    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+  def test_past_questions(self):
+    '''Only questions already published exist, so should be shown.'''
+    question = create_question('Past Question', -3)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+  def test_future_questions(self):
+    '''Only questions with a future pub_date exist, and should not be shown.'''
+    question = create_question('Future Question', 7)
+    response = self.client.get(reverse('polls:index'))
+    self.assertContains(response, 'No polls')
+    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+  def test_past_and_future_questions(self):
+    '''A mixed set of questions already published and with their pub_date in the future exist.
+    Only the ones with their pub_date in the past should be shown.'''
+    past_question = create_question('Past Question', -3)
+    future_question = create_question('Future Question', 3)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(response.context['latest_question_list'], [past_question])
+
+  def test_two_past_questions(self):
+    '''Multiple questions with their pub_date in the past exist, they should all be shown.'''
+    past_question_1 = create_question('Past Question 1', -10)
+    past_question_2 = create_question('Past Question 2', -7)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(response.context['latest_question_list'], [past_question_2, past_question_1]) # the order of questions is important
